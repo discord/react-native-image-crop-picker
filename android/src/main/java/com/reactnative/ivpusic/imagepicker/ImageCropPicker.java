19643bca1026de2a4261d0c49af041aa922c05c1
Charles Xue <charlesx2013@gmail.com>
2025-04-18 12:16:31 -0700

    [android] Fixing permission check issue.

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/ImageCropPicker.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/ImageCropPicker.java
@@ -232,7 +232,7 @@ class ImageCropPicker implements ActivityEventListener {
         List<String> supportedPermissions = new ArrayList<>(requiredPermissions);
 
         // android 11 introduced scoped storage, and WRITE_EXTERNAL_STORAGE no longer works there
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
             supportedPermissions.remove(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
 
